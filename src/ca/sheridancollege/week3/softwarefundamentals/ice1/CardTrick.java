7e2f970ec767b2f34892ad6947496b64302e7f6b
BrarKhush2000 <brar326@sheridanc.on.ca>
2020-01-21 18:52:17 -0500

    - “Finished adding code for 3 and 4 to CardTrick.java”.

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -1,32 +1,50 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Name  -Khushpreet Singh Brar
+Student Number - 991559087
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
+//
+//import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
+
+import java.util.Scanner;
+
 
 /**
- * A class that fills a magic hand of 7 cards with random Card Objects
- * and then asks the user to pick a card and searches the array of cards
- * for the match to the user's card. To be used as starting code in ICE 1
- * @author 
+ * A class that fills a magic hand of 7 cards with random Card Objects and then
+ * asks the user to pick a card and searches the array of cards for the match to
+ * the user's card. To be used as starting code in ICE 1
+ *
+ * @author khush
  */
 public class CardTrick {
-    
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         Card[] magicHand = new Card[7];
         
-        for (int i=0; i<magicHand.length; i++)
+ 
+      
+        for (int i = 0; i < magicHand.length; i++)
+         
         {
+        
+        
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+        double one= (Math.random() *((14-3) +1))+1;
+        int digit = (int) one;
+        double two = (Math.random() *((14-3) +1))+1;
+        int suit = (int) two;
+          
+           c.setValue(digit);
+           c.setSuit(Card.SUITS[suit]);
+           
         }
         
-        //insert code to ask the user for Card value and suit, create their card
+        
+        
+       Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter any card"  + userInput);
         // and search magicHand here
         //Then report the result here
     }
-    
+
 }
